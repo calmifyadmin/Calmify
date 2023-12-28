4ccddfe6a1a4146518c66bbc7c96a34b9da26c8d
calmifyadmin <calmify.ceo@gmail.com>
2023-12-28 21:10:56 +0100

    MultiModular Project

---
+++ core/util/src/main/java/com/lifo/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.lifo.util
+
+object Constants {
+    const val APP_ID = "calmifyapp-uomoz"
+    const val CLIENT_ID = "23546263069-vdutt98mlgpeokvs2122r0rop466mfth.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+
+    const val IMAGES_DATABASE = "images_db"
+    const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
+    const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
+}
---
+++ data/mongo/src/main/java/com/lifo/mongo/repository/MongoRepository.kt
@@ -0,0 +1,22 @@
+package com.lifo.mongo.repository
+
+
+import com.lifo.util.model.Diary
+import com.lifo.util.model.RequestState
+import kotlinx.coroutines.flow.Flow
+import org.mongodb.kbson.ObjectId
+import java.time.LocalDate
+import java.time.ZonedDateTime
+
+typealias Diaries = RequestState<Map<LocalDate, List<Diary>>>
+
+internal interface MongoRepository {
+    fun configureTheRealm()
+    fun getAllDiaries(): Flow<Diaries>
+    fun getFilteredDiaries(zonedDateTime: ZonedDateTime): Flow<Diaries>
+    fun getSelectedDiary(diaryId: ObjectId): Flow<RequestState<Diary>>
+    suspend fun insertDiary(diary: Diary): RequestState<Diary>
+    suspend fun updateDiary(diary: Diary): RequestState<Diary>
+    suspend fun deleteDiary(id: ObjectId): RequestState<Boolean>
+    suspend fun deleteAllDiaries(): RequestState<Boolean>
+}
